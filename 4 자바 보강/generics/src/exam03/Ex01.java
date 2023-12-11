7dc4994193d77db90fd66ef0e1530389e2ca0049
ShinJaeMin98 <jaemin6292@gmail.com>
2023-12-11 18:04:05 +0900

    수업 소스

---
+++ 4 자바 보강/generics/src/exam03/Ex01.java
@@ -0,0 +1,22 @@
+package exam03;
+
+import exam02.Fruit;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        FruitBox<Apple> appleBox = new FruitBox<>();
+
+        appleBox.add(new Apple());  // Apple, Fruit, Object
+        appleBox.add(new Apple());
+        appleBox.add(new Apple());
+
+        Juicier.make(appleBox);
+
+        FruitBox<Fruit> pearBox = new FruitBox<>();
+        pearBox.add(new Pear());
+        pearBox.add(new Pear());
+
+        Juicier.make(pearBox);
+
+    }
+}
---
+++ 4 자바 보강/generics/src/exam03/Juicier.java
@@ -0,0 +1,13 @@
+package exam03;
+
+import exam02.Fruit;
+
+public class Juicier {
+    public static void make(FruitBox<? super Apple> fruitBox) { //  Fruit, Object
+        System.out.println(fruitBox.getItems());
+    }
+
+    public static <T extends Fruit> void make2(FruitBox<T> fruitBox) {
+        System.out.println(fruitBox.getItems());
+    }
+}
